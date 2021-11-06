20ce2433b82df79b6c365f046cd924e75ff4996e
bobo <whb34567@163.com>
2021-11-06 20:58:43 +0800

    初始上传

---
+++ src/main/java/com/bobonode/Test01.java
@@ -0,0 +1,13 @@
+package com.bobonode;
+
+/**
+ * @program: crm001
+ * @description:
+ * @author: Mr.WangBoBo
+ * @create: 2021-11-06 20:54
+ **/
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("hahaha");
+    }
+}

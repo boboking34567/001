ff8dad751390ce0457ad8a75ded81fb70e998df9
bobo <whb34567@163.com>
2021-11-06 21:21:23 +0800

    01修改

--- src/main/java/com/bobonode/Test01.java
+++ src/main/java/com/bobonode/Test01.java
@@ -8,6 +8,10 @@ package com.bobonode;
  **/
 public class Test01 {
     public static void main(String[] args) {
+
         System.out.println("hahaha");
+        for(int i=0;i<=10;i++){
+            System.out.println(i);
+        }
     }
 }
